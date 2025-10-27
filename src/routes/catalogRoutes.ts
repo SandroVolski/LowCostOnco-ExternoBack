40f89634e1fe7e3c53add3a5e974e2b5c1167b5d
SandroVolski <sandroeduardopradovolski@gmail.com>
2025-10-27 03:28:16 -0300

    Limpando cache do Git e aplicando .gitignore

--- src/routes/catalogRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { CatalogController } from '../controllers/catalogController';
-
-const router = Router();
-
-router.get('/principios-ativos', CatalogController.principiosAtivos);
-router.get('/cid10', CatalogController.cid10);
-
-export default router; 
